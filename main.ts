d375fd39c57ae63c42ea15f5e5195ab017f1258f
saleem-301 <adnanyousuf301@gmail.com>
2024-03-09 11:02:59 +0500

    Modified Folder

---
+++ main.ts
@@ -0,0 +1,8 @@
+//Question 6:
+
+let name1 = "    saleem       ";
+
+console.log('I am Learning Typescript\n\tMy name is',name1,'yousuf');
+
+console.log(`I am Learning Typescript\n\tMy name is${name1.trim()}yousuf`);
+
